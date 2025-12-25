953d9026179c00aa5c1b03f868da1f318fdef343
Trungg03 <vohuynhtrung1703@gmail.com>
2025-12-25 10:01:12 +0700

    feat: Update API_URL in Constant.ts and add application.properties for MySQL and Redis configuration

--- next-app-client/src/services/Constant.ts
+++ next-app-client/src/services/Constant.ts
@@ -1,7 +1,7 @@
-// export const API_URL = "http://localhost:3333/api";
+export const API_URL = "http://localhost:3333/api";
 // export const URL_IMAGE = "http://localhost:3333";
 // export const API_URL = "https://big-whole-camel.ngrok-free.app/api";
-export const API_URL = "http://178.128.112.116:80/api";
+// export const API_URL = "http://178.128.112.116:80/api";
 export const URL_IMAGE = "https://res.cloudinary.com";
 export const URL_PPYTHON = "http://178.128.112.116:5000";  // ← Fixed port & host
 
