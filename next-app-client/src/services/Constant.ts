1cebb240fce2a9ce72494cbd04b6978289ff58d6
Phạm Công Tính <congtinh06032003@gmail.com>
2026-01-08 14:54:30 +0700

    Fix

--- next-app-client/src/services/Constant.ts
+++ next-app-client/src/services/Constant.ts
@@ -2,14 +2,14 @@
 // export const URL_IMAGE = "http://localhost:3333";
 // export const API_URL = "https://big-whole-camel.ngrok-free.app/api";
 // export const API_URL = "http://localhost:3333/api";
-// export const API_URL = "http://188.166.222.86:3333/api";
-// export const URL_IMAGE = "https://res.cloudinary.com";
+export const API_URL = "http://188.166.222.86:3333/api";
+export const URL_IMAGE = "https://res.cloudinary.com";
 
 // // Production - API chạy trên server remote
 // // export const API_URL = "http://188.166.222.86:3333/api";
 // // export const API_URL = "http://localhost:3333/api";
 
-// export const URL_PPYTHON = "http://188.166.222.86:5001";  // ← API Gemini trên server (phải dùng IP thực, không dùng localhost trong Docker)
+export const URL_PPYTHON = "http://188.166.222.86:5001";  // ← API Gemini trên server (phải dùng IP thực, không dùng localhost trong Docker)
 // export const URL_PPYTHON = "http://localhost:5001";  // ← API Gemini trên server (phải dùng IP thực, không dùng localhost trong Docker)
 
 // export const API_URL = "http://138.68.234.160:3333/api";
@@ -18,8 +18,8 @@
 // export const URL_PPYTHON = "https://ants-chatbot.onrender.com"
 
 // Cấu hình linh hoạt: Lấy từ Environment Variables, nếu không có thì lấy mặc định (local)
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://188.166.222.86:3333/api";
-export const URL_IMAGE = "https://res.cloudinary.com";
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://188.166.222.86:3333/api";
+// export const URL_IMAGE = "https://res.cloudinary.com";
 
 // Chatbot Python (Gemini)
-export const URL_PPYTHON = process.env.NEXT_PUBLIC_URL_PPYTHON || "http://188.166.222.86:5001";
+// export const URL_PPYTHON = process.env.NEXT_PUBLIC_URL_PPYTHON || "http://188.166.222.86:5001";
--- spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/controllers/ReportLogController.java
+++ spring-ants-ktc/src/main/java/com/ants/ktc/ants_ktc/controllers/ReportLogController.java
@@ -23,7 +23,7 @@ import com.ants.ktc.ants_ktc.config.EnvLoader;
 public class ReportLogController {
 
     private static final String SLACK_TOKEN = EnvLoader.get("SLACK_BOT_TOKEN");
-    private static final String SLACK_CHANNEL_ID = "C09GVNUF5LG";
+    private static final String SLACK_CHANNEL_ID = "C0A0MK03N2Y";
 
     private final RestTemplate restTemplate = new RestTemplate();
 
